a2e020218b641cd21db68c3e612bdb0f44384e38
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2021-01-31 02:01:06 -0500

    adds identity signals

--- identity/apps.py
+++ identity/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class IdentityConfig(AppConfig):
     name = 'identity'
     verbose_name = 'Identity Management'
+
+    def ready(self):
+        import from identity import signals
